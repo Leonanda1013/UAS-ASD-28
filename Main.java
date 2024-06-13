609d7899956406aa7b1e0cec00f695d793fa1cd6
leonanda1013 <leonandaprabowo@gamil.com>
2024-06-13 14:45:38 +0700

    update main

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Kendaraan[] kendaraan = new Kendaraan[10];
+        kendaraan[0] = new Kendaraan("TNKB 1234", "Mobil", 1500, 6);
+        kendaraan[1] = new Kendaraan("TNKB 1234", "Mobil", 1500, 6);
+        kendaraan[2] = new Kendaraan("TNKB 1234", "Mobil", 1500, 6);
+        kendaraan[3] = new Kendaraan("TNKB 1234", "Mobil", 1500, 6);
+        kendaraan[4] = new Kendaraan("TNKB 1234", "Mobil", 1500, 6);
+        kendaraan[5] = new Kendaraan("TNKB 1234", "Mobil", 1500, 6);
+        kendaraan[6] = new Kendaraan("TNKB 1234", "Mobil", 1500, 6);
+        kendaraan[7] = new Kendaraan("TNKB 1234", "Mobil", 1500, 6);
+        kendaraan[8] = new Kendaraan("TNKB 1234", "Mobil", 1500, 6);
+        kendaraan[9] = new Kendaraan("TNKB 1234", "Mobil", 1500, 6);
+
+    }
+}
--- Method.java
+++ Method.java
@@ -3,6 +3,8 @@ public class Method {
         System.out.println("Menu");
         System.out.println("1. Daftar Kendaraan ");
         System.out.println("2. Tampilkan Seluruh Transaksi");
-
+        System.out.println("3. Tampilkan Seluruh Transaksi");
+        System.out.println("4. Ururtkan Transaksi Berdasarkan Nama Pemilik");
+        System.out.println("5. Keluar");
     }
 }
